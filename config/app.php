e3ecee625dc05e4eefe2452739e81daedb3eff51
yinjiajiu <vip_yinjiajiu@163.com>
2019-05-11 22:51:39 +0800

    添加自定义异常和api接口统一处理,修改.gitignore,删除无用的txt和md

--- config/app.php
+++ config/app.php
@@ -33,7 +33,7 @@ return [
     // 域名绑定（自动多应用模式有效）
     'domain_bind'           => [],
     // 禁止URL访问的应用列表（自动多应用模式有效）
-    'deny_app_list'         => [],
+    'deny_app_list'         => ['common'],
     // 默认应用
     'default_app'           => 'admin',
     // 默认时区
